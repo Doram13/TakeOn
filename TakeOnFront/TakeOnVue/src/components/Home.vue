b3e6c1ae0cb21c7b9779546d3616d5e7a330217a
Doram13 <1991doram@gmail.com>
2019-05-27 15:10:49 +0200

    Vue front added

---
+++ TakeOnFront/TakeOnVue/src/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <b-card no-body>
+      <b-tabs content-class="mt-3" fill card>
+        <b-tab title="Routine"><one-day :text="posts[0].text"/></b-tab>
+        <b-tab title="Peer"><one-day :text="posts[1].text"/></b-tab>
+        <b-tab title="Journal"><free-style/></b-tab>
+      </b-tabs>
+    </b-card>
+  </div>
+</template>
+<script>
+import OneDayTab from '@/components/OneDayTab';
+import PostService from '@/api-services/journal.service';
+import FreestyleText from '@/components/FreestyleText';
+
+export default {
+  name: 'Home',
+  components: {
+    'one-day': OneDayTab,
+    'free-style': FreestyleText,
+  },
+  created() {
+    PostService.create({ Description: 'this is a journal' });
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1,
+          date: '2019-05-10',
+          text: [
+            { id: 1, question: 'Question 1', answer: 'daily answer' },
+            { id: 2, question: 'Q2', answer: 'daily asdlfkj séodifh aséldk fhaséd hfaosidh fanswer' },
+            { id: 3, question: 'question 3', answer: 'daily answer' },
+            { id: 4, question: 'question 4', answer: 'daily asdf' },
+            { id: 5, question: 'question 5', answer: 'daiasdfasdfly answer' }] },
+        { id: 2,
+          date: '2019-05-11',
+          text: [
+            { id: 1, question: 'peerq1', answer: 'daily answer' },
+            { id: 2, question: 'questionpeer2', answer: 'daily answer' },
+            { id: 3, question: 'questionpee3', answer: 'asdf answer' },
+            { id: 4, question: 'questionasdf', answer: 'daily answer' },
+            { id: 5, question: 'questionsdf', answer: 'daily answer' }] },
+        { id: 3,
+          date: '2019-05-12',
+          text: [
+            { id: 1, question: 'daily question', answer: 'daily answer' },
+            { id: 2, question: 'daily question', answer: 'daily answer' },
+            { id: 3, question: 'daily question', answer: 'dailyasdfasdf answer' },
+            { id: 4, question: 'daily question', answer: 'daily answer' },
+            { id: 5, question: 'daily question', answer: 'daily answer' }] },
+        { id: 4,
+          date: '2019-05-13',
+          text: [
+            { id: 1, question: 'daily question', answer: 'daily answer' },
+            { id: 2, question: 'daily question', answer: 'daily answer' },
+            { id: 3, question: 'dailyasdfasdf question', answer: 'daily answer' },
+            { id: 4, question: 'daily question', answer: 'daily answer' },
+            { id: 5, question: 'daily quasdfasdfestion', answer: 'daily answer' }] },
+        { id: 5,
+          date: '2019-05-14',
+          text: [
+            { id: 1, question: 'daily question', answer: 'daily answer' },
+            { id: 2, question: 'dailasdfasdfy question', answer: 'daily answer' },
+            { id: 3, question: 'daily question', answer: 'daily answer' },
+            { id: 4, question: 'daily question', answer: 'daily answer' },
+            { id: 5, question: 'daily question', answer: 'dailasdfasdfy answer' }] },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
---
+++ TakeOnFront/TakeOnVue/src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand :to="{ name: 'Home' }">TakeOff</b-navbar-brand>
+
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <datepicker bootstrap-styling="true" :value="date">
+          </datepicker>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">HU</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template slot="button-content"><em>User</em></template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<script>
+import Datepicker from 'vuejs-datepicker';
+import PostService from '@/api-services/post.service';
+
+export default {
+  name: 'Navbar',
+  components: { Datepicker },
+  data() {
+    return {
+      date: new Date(Date.now()),
+    };
+  },
+  methods: {
+    onClick() {
+      PostService.create({ Id: 1, Description: 'this is a journal' });
+    },
+  },
+
+};
+</script>
+
+<style scoped>
+
+</style>
